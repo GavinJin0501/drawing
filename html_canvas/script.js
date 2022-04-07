fcbad17db81bcadc1bc7e41f0279113e6998c25a
JJY <jj2915@nyu.edu>
2022-04-07 11:43:09 -0400

    fix bug

--- html_canvas/script.js
+++ html_canvas/script.js
@@ -4,10 +4,11 @@ const canva1 = canvas[0];
 const canva2 = canvas[1];
 const context1 = canva1.getContext("2d");
 const context2 = canva2.getContext("2d");
-const width1 = canva1.width;
-const height1 = canva1.height;
-const width2 = canva2.width;
-const height2 = canva2.height;
+const scale = window.devicePixelRatio;
+let width1;
+let height1;
+let width2;
+let height2;
 const r = 150;
 const hourPtr = r - 90;
 const minutePtr = hourPtr + 30;
@@ -18,27 +19,31 @@ let fps = 0;
 
 
 function setup() {
-    canva1.style.width = canva1.width + 'px';
-    canva1.style.height = canva1.height + 'px';
-    canva2.style.width = canva2.width + 'px';
-    canva2.style.height = canva2.height + 'px';
+    width1 = canva1.width;
+    height1 = canva1.height;
+    width2 = canva2.width;
+    height2 = canva2.height;
 
-    const scale = window.devicePixelRatio;
-    canva1.width *= scale;
-    canva1.height *= scale;
-    canva2.width *= scale;
-    canva2.height *= scale;
+    canva1.style.width = width1 + 'px';
+    canva1.style.height = height1 + 'px';
+    canva2.style.width = width2 + 'px';
+    canva2.style.height = height2 + 'px';
+
+    canva1.width = width1 * scale;
+    canva1.height = height1 * scale;
+    canva2.width = width2 * scale;
+    canva2.height = height2 * scale;
 
     context1.scale(scale, scale);
     context2.scale(scale, scale);
-    context2.drawImage(image, 0, 0, width2, height2);
+    context2.drawImage(image, 0, 0, 600, 400);
 }
 
 
 function draw() {
     if (fps % 1 === 0) {
         // draw a static circle
-        context1.clearRect(0, 0, width1, height1);
+        context1.clearRect(0, 0, 600, 400);
         context1.strokeStyle = "black";
         context1.lineWidth = 2;
         context1.fillStyle = "white";
@@ -58,7 +63,6 @@ function draw() {
         context1.lineTo(width1 / 2 + Math.sin(hour % 12 * Math.PI / 6) * hourPtr, height1 / 2 - Math.cos(hour % 12 * Math.PI / 6) * hourPtr);
         context1.stroke();
         context1.fill();
-        console.log(hour, minute);
         hour = (hour + Math.floor((minute + 1) / 60)) % 24;
         minute = (minute + 1) % 60;
 
@@ -85,7 +89,8 @@ function draw() {
 
     if (fps % 6 === 0) {
         // display picture
-        const imageData = context2.getImageData(0, 0, width2, height2);
+        const imageData = context2.getImageData(0, 0, canva2.width, canva2.height);
+        // context2.clearRect(0, 0, 600, 400);
         let data = imageData.data;
         
         for (let i = 0; i < data.length; i += 4) {
