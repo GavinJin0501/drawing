ff8d24ec38f5595c7ae665b6764fb4bb7728475a
JJY <jj2915@nyu.edu>
2022-04-25 16:20:27 -0400

    fix firefox bugs

--- final_project/static/js/drawing_busy.js
+++ final_project/static/js/drawing_busy.js
@@ -1,7 +1,7 @@
 let winWidth;   // window width
 let winHeight;  // window height
 const ghostArr = [];
-const ghostNum = 10;
+const ghostNum = 40;
 const body = document.querySelector("body");
 
 class Ghost {
@@ -9,41 +9,11 @@ class Ghost {
         // create svg element and append it to the body
         const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         const use = document.createElementNS("http://www.w3.org/2000/svg", "use");
-        svg.class = "svgs";
         svg.id = "ghost";
         use.setAttribute("href", "./static/ghost.svg#svg2");
         svg.appendChild(use);
         body.appendChild(svg);
         this.element = svg;
-
-        // set initial random position
-        const p = Math.floor(Math.random() * 2);
-        let rect = this.element.getBoundingClientRect();
-        const rdY = Math.floor(Math.random() * (winHeight - rect.height));
-        this.X = 0;
-        this.Y = 0;
-        this.speed = 6;
-        if (p === 0) { // start form left
-            this.direction = this.speed;
-            this.element.style.left = -rect.width / 2;
-            this.element.style.top = rdY;
-            this.X = - rect.width / 2;
-            this.Y = rdY;
-        } else { // start form right
-            this.direction = -this.speed;
-            this.element.style.left = winWidth - rect.width / 2;
-            this.element.style.top = rdY;
-            this.X = winWidth - rect.width / 2;
-            this.Y = rdY;
-        }
-        
-        rect = this.element.getBoundingClientRect();
-        this.centerX = rect.x + rect.width / 2;
-        this.centerY = rect.y + rect.height / 2;
-        this.dstX = 0;
-        this.dstY = 0;
-
-        requestAnimationFrame(() => this.walk());
     }
 
     walk() {
@@ -56,11 +26,44 @@ class Ghost {
         if (this.direction < 0 && this.X < -rect.width) {
             this.X = winWidth - rect.width / 2;
         }
-        this.element.style.left = this.X;
+        this.element.style.left = `${this.X}px`;
 
-        requestAnimationFrame(() => this.walk());
+        this.animation = requestAnimationFrame(() => this.walk());
     }
 
+    startWalk() {
+        // set initial random position
+        const p = Math.floor(Math.random() * 2);
+        let rect = this.element.getBoundingClientRect();
+        const rdX = Math.floor(Math.random() * winWidth - rect.width / 2);
+        const rdY = Math.floor(Math.random() * (winHeight - rect.height));
+        this.X = rdX;
+        this.Y = rdY;
+        this.speed = winWidth / 2000 * 6;
+        if (p === 0) { // start form left
+            this.direction = this.speed;
+            this.element.style.left = `${-rect.width / 2}px`;
+            this.element.style.top = `${rdY}px`;
+            // this.X = - rect.width / 2;
+        } else { // start form right
+            this.direction = -this.speed;
+            this.element.style.left = `${winWidth - rect.width / 2}px`;
+            this.element.style.top = `${rdY}px`;
+            // this.X = winWidth - rect.width / 2;
+        }
+        
+        rect = this.element.getBoundingClientRect();
+        this.centerX = rect.x + rect.width / 2;
+        this.centerY = rect.y + rect.height / 2;
+        this.dstX = 0;
+        this.dstY = 0;
+
+        this.animation = requestAnimationFrame(() => this.walk());
+    }
+
+    cancelWalk() {
+        cancelAnimationFrame(this.animation);
+    }
 }
 
 
@@ -68,11 +71,21 @@ function setWinSize() {
     winWidth = window.innerWidth;
     winHeight = window.innerHeight;
 
-    // init ghosts
-    for (let i = 0; i < ghostNum; i++) {
-        const g = new Ghost();
-        ghostArr.push(g);
+    
+    if (ghostArr.length === 0) {
+        // init ghosts
+        for (let i = 0; i < ghostNum; i++) {
+            const g = new Ghost();
+            g.startWalk();
+            ghostArr.push(g);
+        }
+    } else {
+        for (let i = 0; i < ghostNum; i++) {
+            ghostArr[i].cancelWalk();
+            ghostArr[i].startWalk();
+        }
     }
+    
 }
 
 
